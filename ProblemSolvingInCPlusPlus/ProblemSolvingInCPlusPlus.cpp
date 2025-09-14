b92fbfa6ef4f365e8e45854616a5dcca1a0acaa8
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-14 20:09:23 +0300

    solve evalRPN problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -605,8 +605,31 @@ public:
 	}
 };
 
+int evalRPN(vector<string>& tokens) {
+	stack<int> st;
+	for (int i = 0; i < tokens.size(); i++)
+	{
+		if (tokens.at(i) == "+" || tokens.at(i) == "*" || tokens.at(i) == "-" || tokens.at(i) == "/")
+		{
+			int b = st.top(); 
+			st.pop();
+			
+			int a = st.top(); 
+			st.pop();
+			
+			if (tokens[i] == "+") st.push(a + b);
+			else if (tokens[i] == "-") st.push(a - b);
+			else if (tokens[i] == "*") st.push(a * b);
+			else if (tokens[i] == "/") st.push(a / b);
+		}
+		else
+			st.push(stoi(tokens[i]));
+	}
+	return st.top();
+}
+
 int main()
-{
+{ 
 
 	system("pause>0");
 }
