c87327bc2cae4de23dfc440640ff97f6469b5d3f
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-16 00:59:55 +0300

    solve mergeTwoLists problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -642,6 +642,30 @@ bool hasCycle(ListNode* head) {
 	return false;
 }
 
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+	ListNode* dummy = new ListNode(0);
+	ListNode* current = dummy;
+	while (list1 && list2)
+	{
+		if (list1->val < list2->val)
+		{
+			current->next = list1;
+			list1 = list1->next;
+		}
+		else
+		{
+			current->next = list2;
+			list2 = list2->next;
+		}
+		current = current->next;
+	}
+	if (list1)
+		current->next = list1;
+	if (list2)
+		current->next = list2;
+	return dummy->next;
+}
+
 int main()
 { 
 
