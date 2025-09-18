e5523e0493a592ff09574347d7efc8f5443b8bd3
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-18 14:48:37 +0300

    solve isSymmetric problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -803,6 +803,19 @@ TreeNode* invertTree(TreeNode* root) {
 	return root;
 }
 
+
+bool isMirror(TreeNode* t1, TreeNode* t2) {
+	if (!t1 || !t2) return false;
+	if (!t1 && !t2) return true;
+	if (t1->val != t2->val) return false;
+	return isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+	if (!root) return false;
+	return isMirror(root->left, root->right);
+}
+
 int main()
 { 
 
