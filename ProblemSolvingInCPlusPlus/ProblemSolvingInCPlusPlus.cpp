3296c5e820ced85b2c54f09742d78d64bcb65b91
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-18 13:52:59 +0300

    solve isSameTree problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -778,6 +778,16 @@ int maxDepth(TreeNode* root) {
 	return max(leftDepth, rightDepth) + 1;
 }
 
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	if (!p && !q)
+		return true;
+	if (!p || !q)
+		return false;
+	if (p->val != q->val)
+		return false;
+	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
 int main()
 { 
 
