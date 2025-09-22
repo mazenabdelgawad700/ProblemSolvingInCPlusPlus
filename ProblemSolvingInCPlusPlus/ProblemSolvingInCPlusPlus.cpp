0c4f1c1c1d670c786f94ace5de73673d1e4aa035
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-22 14:49:01 +0300

    solve rightSideView problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -900,6 +900,33 @@ int sumNumbers(TreeNode* root) {
 	return sum;
 }
 
+vector<int> rightSideView(TreeNode* root)
+{
+	TreeNode* temp = root;
+	vector<int> result;
+	if (!temp) return result;
+	queue<TreeNode*> q;
+
+	q.push(temp);
+	
+	while (!q.empty())
+	{
+		int size = q.size();
+		for (int i = 0; i < size; i++)
+		{
+			TreeNode* node = q.front();
+			q.pop();
+			if (i == size - 1)
+				result.push_back(node->val);
+			if (node->left)
+				q.push(node->left);
+			if (node->right)
+				q.push(node->right);
+		}
+	}
+	return result;
+}
+
 int main()
 { 
 
