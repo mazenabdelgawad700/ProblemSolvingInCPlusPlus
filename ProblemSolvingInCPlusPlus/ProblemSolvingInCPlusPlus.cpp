f512c67e95f5b8a69424885f97aae3ca8d22ce16
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-26 14:21:07 +0300

    solve repeatedSubstringPattern problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1375,8 +1375,24 @@ int arrangeCoins(int n) {
 	return k - 1;
 }
 
+bool repeatedSubstringPattern(string s) {
+	int n = s.length();
+	for (int i = 1; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			string substring = s.substr(0, i);
+			string repeated = "";
+			for (int j = 0; j < n / i; j++)
+				repeated += substring;
+			if (repeated == s)
+				return true;
+		}
+	}
+	return false;
+}
+
 int main()
 { 
-	cout << arrangeCoins(1) << endl;
 	system("pause>0");
 }
