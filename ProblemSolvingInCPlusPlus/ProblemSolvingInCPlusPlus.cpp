fc12bc6da8084d2d6713b8317ff0a8b4d6c4c92d
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-26 03:32:28 +0300

    solve levelOrder, arrangeCoins problems

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1341,7 +1341,42 @@ vector<string> fizzBuzz(int n) {
 	return result;
 }
 
+vector<vector<int>> levelOrder(TreeNode* root) {
+	vector<vector<int>> result;
+	if (!root) return result;
+	queue<TreeNode*> q;
+	q.push(root);
+	while(!q.empty())
+	{
+		int size = q.size();
+		vector<int> level;
+		for (int i = 0; i < size; i++)
+		{
+			TreeNode* node = q.front();
+			q.pop();
+			level.push_back(node->val);
+			if (node->left)
+				q.push(node->left);
+			if (node->right)
+				q.push(node->right);
+		}
+		result.push_back(level);
+	}
+	return result;
+}
+
+int arrangeCoins(int n) {
+	int k = 0;
+	while (n >= 0)
+	{
+		k++;
+		n -= k;
+	}
+	return k - 1;
+}
+
 int main()
 { 
+	cout << arrangeCoins(1) << endl;
 	system("pause>0");
 }
