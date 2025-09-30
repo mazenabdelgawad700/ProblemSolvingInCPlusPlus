63c759c6bfc506457cc61b61cf441c32631249e1
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-09-30 09:58:37 +0300

    solve tribonacci problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1483,11 +1483,21 @@ bool detectCapitalUse(string word)
 	return true;
 }
 
+map<int, int> storage;
+int tribonacci(int n) {
+	if (n == 0)
+		return 0;
+	if (n == 1 || n == 2)
+		return 1;
+	if (storage.find(n) != storage.end())
+		return storage[n];
 
+	storage[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+
+	return storage[n];
+}
 
 int main()
 { 
-	cout << detectCapitalUse("mL") << endl;
-
 	system("pause>0");
 }
