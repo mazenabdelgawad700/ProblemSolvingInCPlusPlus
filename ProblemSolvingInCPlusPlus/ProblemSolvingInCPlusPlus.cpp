eef93f0dab2db0f710aa4c4dc2fe39df4152d538
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2025-10-07 14:02:31 +0200

    solve MedianFinder problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1575,6 +1575,34 @@ bool isSubtree(TreeNode* root, TreeNode* subRoot)
 	return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
 }
 
+class MedianFinder {
+public:
+	priority_queue<int> low;
+	priority_queue<int, vector<int>, greater<int>> high;
+	MedianFinder() {}
+
+	void addNum(int num) 
+	{
+		low.push(num);
+
+		high.push(low.top());
+		low.pop();
+
+		if (low.size() < high.size())
+		{
+			low.push(high.top());
+			high.pop();
+		}
+	}
+
+	double findMedian() {
+		if (low.size() > high.size())
+			return low.top();
+
+		return (low.top() + high.top()) / 2.0;
+	}
+};
+
 int main()
 { 
 	system("pause>0");
