5c98815cd14bb223c5f657edcfa638c2267dfc6a
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2025-10-08 20:18:44 +0200

    solve countPairs problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1603,6 +1603,24 @@ public:
 	}
 };
 
+int countPairs(vector<int>& nums, int k) {
+	int counter = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		for (int j = i + 1; j < nums.size(); j++)
+		{
+			if (nums[i] == nums[j])
+			{
+				if ((i * j) % k == 0)
+					counter++;
+			}
+		}
+	}
+	return counter;
+}
+
+
+
 int main()
 { 
 	system("pause>0");
