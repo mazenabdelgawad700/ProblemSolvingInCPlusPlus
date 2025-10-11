497e98c189fa861cd517ce9e830bbdff482dca36
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2025-10-11 13:35:39 +0300

    solve numTeams by DP

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1666,7 +1666,7 @@ bool canPartition(vector<int>& nums) {
 }
 
 int numTeams(vector<int>& rating) {
-	int teams_counter = 0;
+	/*int teams_counter = 0;
 	for (int i = 0; i < rating.size(); i++)
 	{
 		for (int j = i + 1; j < rating.size(); j++)
@@ -1680,7 +1680,37 @@ int numTeams(vector<int>& rating) {
 			}
 		}
 	}
-	return teams_counter;
+	return teams_counter;*/
+	int n = rating.size();
+	int mx = *max_element(rating.begin(), rating.end());
+	vector<int> pre1(mx + 1, 0), pre2(mx + 1, 0);
+
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = j + 1; k < n; k++)
+		{
+			if(rating[k] > rating[j])
+				pre1[rating[j]]++;
+			else if (rating[k] < rating[j])
+				pre2[rating[j]]++;
+		}
+	}
+
+
+	int cnt = 0;
+
+	for(int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if(rating[i] < rating[j])
+				cnt += pre1[rating[j]];
+			if (rating[i] > rating[j])
+				cnt += pre2[rating[j]];
+		}
+	}
+	return cnt;
 }
 
 
