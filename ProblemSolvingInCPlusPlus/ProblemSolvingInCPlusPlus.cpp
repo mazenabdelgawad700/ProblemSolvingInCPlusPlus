46605e411191dce30d66277a11bbf7b61fcbdc08
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2025-10-19 17:38:08 +0300

    solve strStr problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1814,6 +1814,21 @@ int numUniqueEmails(vector<string>& emails) {
 	return counter;
 }
 
+int strStr(string haystack, string needle) {
+	if (needle.length() > haystack.length())
+		return -1;
+
+	for (size_t i = 0; i <= haystack.length() - needle.length(); i++)
+	{
+		if (haystack.substr(i, needle.length()) == needle)
+			return i;
+	}
+
+	return -1;
+}
+
+
+
 int main()
 {
 	system("pause>0");
