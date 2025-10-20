b179a598a0373f9c541b30cfd4ff31c477a429eb
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2025-10-20 14:04:38 +0300

    solve pivotIndex problem

--- ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
+++ ProblemSolvingInCPlusPlus/ProblemSolvingInCPlusPlus.cpp
@@ -1852,6 +1852,22 @@ public:
 	}
 };
 
+int pivotIndex(vector<int>& nums) {
+	for (int i = 0; i < nums.size(); i++)
+	{
+		int left_sum = 0;
+		for (int l = 0; l < i; l++)
+			left_sum += nums[l];
+
+		int right_sum = 0;
+		for (int r = i + 1; r < nums.size(); r++)
+			right_sum += nums[r];
+		
+		if (left_sum == right_sum)
+			return i;
+	}
+	return -1;
+}
 
 int main()
 {
